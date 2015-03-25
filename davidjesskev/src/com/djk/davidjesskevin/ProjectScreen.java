271b18316519c4d72f48d16144234aca5eb2b2d0
Cptniiii <davidja@vt.edu>
2015-03-25 16:31:57 -0400

    This is really annoying

---
+++ davidjesskev/src/com/djk/davidjesskevin/ProjectScreen.java
@@ -0,0 +1,16 @@
+package com.djk.davidjesskevin;
+
+import sofia.app.Screen;
+
+// -------------------------------------------------------------------------
+/**
+ *  Project screen.
+ *
+ *  @author David
+ *  @version Mar 25, 2015
+ */
+public class ProjectScreen
+    extends Screen
+{
+
+}
