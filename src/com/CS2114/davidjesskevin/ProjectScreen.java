98ac1baac0158f370c7d5bba9f2b88e6ee975915
Cptniiii <davidja@vt.edu>
2015-03-25 01:17:10 -0400

    I don't think this build is stable. It crashes at launch.

--- src/com/CS2114/davidjesskevin/ProjectScreen.java
+++ src/com/CS2114/davidjesskevin/ProjectScreen.java
@@ -1,17 +1,16 @@
 package com.CS2114.davidjesskevin;
 
-import com.CS2114.davidjesskevin.Kitten;
 import sofia.app.ShapeScreen;
 
-//-------------------------------------------------------------------------
+// -------------------------------------------------------------------------
 /**
-* The visual representation of the maze.
-*
-* @author David Atkinson (davidja)
-* @version 2015.03.08
-*/
+ * The visual representation of the maze.
+ *
+ * @author David Atkinson (davidja)
+ * @version 2015.03.08
+ */
 public class ProjectScreen
- extends ShapeScreen
+    extends ShapeScreen
 {
 
     // ----------------------------------------------------------
