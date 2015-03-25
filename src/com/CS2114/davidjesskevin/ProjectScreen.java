6a0f721e75f7aac8538efa8759430e81880cf905
Cptniiii <davidja@vt.edu>
2015-03-24 23:38:12 -0400

    Changed: deleted a file added ProjectScreen.java edited activity_main.xml

---
+++ src/com/CS2114/davidjesskevin/ProjectScreen.java
@@ -0,0 +1,25 @@
+package com.CS2114.davidjesskevin;
+
+import sofia.app.ShapeScreen;
+
+//-------------------------------------------------------------------------
+/**
+* The visual representation of the maze.
+*
+* @author David Atkinson (davidja)
+* @version 2015.03.08
+*/
+public class ProjectScreen
+ extends ShapeScreen
+{
+
+    // ----------------------------------------------------------
+    /**
+     * initialize method for the screen.
+     */
+    public void initialize()
+    {
+        // TODO Auto-generated stub.
+    }
+
+}
